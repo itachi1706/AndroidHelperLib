a1972c3d95a4ca2668516dfc64a321b21ff7a526
Kenneth Soh <kennethsohyq@gmail.com>
2021-05-19 19:00:33 +0800

    Added long and indefinite toasts and snackbars as supported
    Closes #4
    
    Took 2 minutes

--- helperlib/src/main/java/com/itachi1706/helperlib/utils/NotifyUserUtil.kt
+++ helperlib/src/main/java/com/itachi1706/helperlib/utils/NotifyUserUtil.kt
@@ -12,13 +12,35 @@ import com.itachi1706.helperlib.R
  */
 object NotifyUserUtil {
     /**
-     * Creates a short dismissable snackbar object
+     * Creates a short dismissible snackbar popup
      * @param currentLayout Current View Layout
      * @param message Message in Snackbar
      */
     @JvmStatic
     fun showShortDismissSnackbar(currentLayout: View, message: CharSequence) {
         Snackbar.make(currentLayout, message, Snackbar.LENGTH_SHORT)
+            .setAction(R.string.snackbar_action_dismiss) { }.show()
+    }
+
+    /**
+     * Creates a long dismissible snackbar popup
+     * @param currentLayout Current View Layout
+     * @param message Message in Snackbar
+     */
+    @JvmStatic
+    fun showLongDismissSnackbar(currentLayout: View, message: CharSequence) {
+        Snackbar.make(currentLayout, message, Snackbar.LENGTH_LONG)
+            .setAction(R.string.snackbar_action_dismiss) { }.show()
+    }
+
+    /**
+     * Creates a permanent snackbar popup
+     * @param currentLayout Current View Layout
+     * @param message Message in Snackbar
+     */
+    @JvmStatic
+    fun showIndefiniteSnackbar(currentLayout: View, message: CharSequence) {
+        Snackbar.make(currentLayout, message, Snackbar.LENGTH_INDEFINITE)
             .setAction(R.string.snackbar_action_dismiss) { }.show()
     }
 
@@ -30,5 +52,15 @@ object NotifyUserUtil {
     @JvmStatic
     fun createShortToast(context: Context, message: CharSequence) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Create a Long Toast Message
+     * @param context Application Context
+     * @param message Message to display in Toast
+     */
+    @JvmStatic
+    fun createLongToast(context: Context, message: CharSequence) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
