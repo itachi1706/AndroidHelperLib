3bbf9c945da4c2ab1dfcd3f12c7ee87e6af134ec
Kenneth Soh <kenneth@itachi1706.com>
2024-01-31 01:21:41 +0800

    refactor: Simply codebase

--- helperlib/src/main/java/com/itachi1706/helperlib/helpers/ApiCallsHelper.kt
+++ helperlib/src/main/java/com/itachi1706/helperlib/helpers/ApiCallsHelper.kt
@@ -3,13 +3,18 @@ package com.itachi1706.helperlib.helpers
 import android.content.Context
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.itachi1706.helperlib.objects.ApiResponse
 import kotlinx.serialization.json.Json
 
 @Suppress("unused")
-class ApiCallsHelper(context: Context, private val baseUrl: String = DEFAULT_URL, private val tag: String = "DEFAULT") {
+class ApiCallsHelper(
+    context: Context,
+    private val baseUrl: String = DEFAULT_URL,
+    private val tag: String = "DEFAULT"
+) {
 
     // Set up volley request queue
     private val queue: RequestQueue = Volley.newRequestQueue(context)
@@ -24,68 +29,56 @@ class ApiCallsHelper(context: Context, private val baseUrl: String = DEFAULT_URL
 
     fun makeGetCall(path: String, listener: ApiCallListener) {
         // Make a HTTP GET Call to the URL and path with Volley
-        val url = "$baseUrl/$path"
-
-        val request = StringRequest(Request.Method.GET, url, { response ->
-            listener.onApiCallSuccess(serializeResponse(response))
-        }, { error ->
-            listener.onApiCallError(error.message ?: UNKNOWN_ERROR)
-        })
-        request.tag = tag
-        queue.add(request)
+        internalCallHandling(Request.Method.GET, path, null, listener)
     }
 
-    fun makePostCall(path: String, data: String, listener: ApiCallListener) {
+    fun makePostCall(path: String, data: String?, listener: ApiCallListener) {
         // Make a HTTP POST Call to the URL and path
-        val url = "$baseUrl/$path"
-
-        val request = object : StringRequest(Method.POST, url, { response ->
-            listener.onApiCallSuccess(serializeResponse(response))
-        }, { error ->
-            listener.onApiCallError(error.message ?: UNKNOWN_ERROR)
-        }) {
-            override fun getBody(): ByteArray {
-                return data.toByteArray()
-            }
-
-            override fun getBodyContentType(): String {
-                return "application/json"
-            }
-        }
-        request.tag = tag
-        queue.add(request)
+        internalCallHandling(Request.Method.POST, path, data, listener)
     }
 
-    fun makePutCall(path: String, data: String, listener: ApiCallListener) {
+    fun makePutCall(path: String, data: String?, listener: ApiCallListener) {
         // Make a HTTP PUT Call to the URL and path
-        val url = "$baseUrl/$path"
-
-        val request = object : StringRequest(Method.PUT, url, { response ->
-            listener.onApiCallSuccess(serializeResponse(response))
-        }, { error ->
-            listener.onApiCallError(error.message ?: UNKNOWN_ERROR)
-        }) {
-            override fun getBody(): ByteArray {
-                return data.toByteArray()
-            }
-
-            override fun getBodyContentType(): String {
-                return "application/json"
-            }
-        }
-        request.tag = tag
-        queue.add(request)
+        internalCallHandling(Request.Method.PUT, path, data, listener)
     }
 
     fun makeDeleteCall(path: String, listener: ApiCallListener) {
         // Make a HTTP DELETE Call to the URL and path
+        internalCallHandling(Request.Method.DELETE, path, null, listener)
+    }
+
+    private fun internalCallHandling(
+        method: Int,
+        path: String,
+        data: String?,
+        listener: ApiCallListener
+    ) {
+        // Make a HTTP DELETE Call to the URL and path
         val url = "$baseUrl/$path"
 
-        val request = StringRequest(Request.Method.DELETE, url, { response ->
+        val successListener = Response.Listener { response: String ->
             listener.onApiCallSuccess(serializeResponse(response))
-        }, { error ->
-            listener.onApiCallError(error.message ?: UNKNOWN_ERROR)
-        })
+        }
+        val failedListener = Response.ErrorListener { error ->
+            listener.onApiCallError(
+                error?.message ?: UNKNOWN_ERROR
+            )
+        }
+
+        val request: StringRequest = if (data != null) {
+            object : StringRequest(method, url, successListener, failedListener) {
+                override fun getBody(): ByteArray {
+                    return data.toByteArray()
+                }
+
+                override fun getBodyContentType(): String {
+                    return "application/json"
+                }
+            }
+        } else {
+            StringRequest(method, url, successListener, failedListener)
+        }
+
         request.tag = tag
         queue.add(request)
     }
