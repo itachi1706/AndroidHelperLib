da4fb3fe553ea3b81864127624fd71611c6f3aee
Kenneth Soh <kenneth@itachi1706.com>
2025-07-21 19:00:24 +0800

    chore: Added checks to ensure that Inset Type is only specific values

--- helperlib/src/main/java/com/itachi1706/helperlib/helpers/EdgeToEdgeHelper.kt
+++ helperlib/src/main/java/com/itachi1706/helperlib/helpers/EdgeToEdgeHelper.kt
@@ -11,11 +11,19 @@ import androidx.core.view.WindowInsetsCompat
 @RequiresApi(Build.VERSION_CODES.R)
 object EdgeToEdgeHelper {
 
+    /**
+     * Disable the window system windows to allow edge to edge layout
+     * @param window The window to disable system windows on
+     */
     @JvmStatic
     fun disableWindowSystemWindows(window: Window) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 
+    /**
+     * Set the view to be edge to edge with system bars
+     * @param view The view to set the insets on
+     */
     @JvmStatic
     fun setViewEdgeToEdge(view: android.view.View) {
         setViewEdgeToEdge(view, WindowInsetsCompat.Type.systemBars())
@@ -24,10 +32,10 @@ object EdgeToEdgeHelper {
     /**
      * Set the view to be edge to edge with a specific inset type
      * @param view The view to set the insets on
-     * @param insetType The type of inset to apply from [WindowInsetsCompat.Type], e.g. [WindowInsetsCompat.Type.systemBars()]
+     * @param insetType The type of insets to apply, e.g. [WindowInsetsCompat.Type.systemBars]
      */
     @JvmStatic
-    fun setViewEdgeToEdge(view: android.view.View, insetType: Int) {
+    fun setViewEdgeToEdge(view: android.view.View, @WindowInsetsCompat.Type.InsetsType insetType: Int) {
         ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInsets ->
             val insets = windowInsets.getInsets(insetType)
             v.setPadding(insets.left, insets.top, insets.right, insets.bottom)
