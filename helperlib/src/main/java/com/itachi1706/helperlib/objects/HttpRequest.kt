405b9443e6db3d16df7598367bed3194542f2b7a
Kenneth Soh <kenneth@itachi1706.com>
2025-10-02 10:36:42 +0800

    refactor: Use object directly

--- helperlib/src/main/java/com/itachi1706/helperlib/objects/HttpRequest.kt
+++ helperlib/src/main/java/com/itachi1706/helperlib/objects/HttpRequest.kt
@@ -2,23 +2,21 @@ package com.itachi1706.helperlib.objects
 
 import com.android.volley.Request
 
-class HttpRequest {
+object HttpRequest {
     enum class Method {
         GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, TRACE
     }
 
-    companion object {
-        fun mapToVolleyMethods(method: Method): Int {
-            return when (method) {
-                Method.GET -> Request.Method.GET
-                Method.POST -> Request.Method.POST
-                Method.PUT -> Request.Method.PUT
-                Method.DELETE -> Request.Method.DELETE
-                Method.PATCH -> Request.Method.PATCH
-                Method.HEAD -> Request.Method.HEAD
-                Method.OPTIONS -> Request.Method.OPTIONS
-                Method.TRACE -> Request.Method.TRACE
-            }
+    fun mapToVolleyMethods(method: Method): Int {
+        return when (method) {
+            Method.GET -> Request.Method.GET
+            Method.POST -> Request.Method.POST
+            Method.PUT -> Request.Method.PUT
+            Method.DELETE -> Request.Method.DELETE
+            Method.PATCH -> Request.Method.PATCH
+            Method.HEAD -> Request.Method.HEAD
+            Method.OPTIONS -> Request.Method.OPTIONS
+            Method.TRACE -> Request.Method.TRACE
         }
     }
 }
